6e6688968b8367e5a970d18560573a044f6c8102
Christian996 <chris.schmid996@yahoo.de>
2017-01-23 12:10:00 +0100

    Add Hauptmenü von Yannick and Soundtrack
    Hauptmenü von Yannick hinzugefügt
    Final Soundtrack für das erste Level hinzugefügt

--- Kitchen Panic/Assets/Scripts/CharacterMovement.cs
+++ Kitchen Panic/Assets/Scripts/CharacterMovement.cs
@@ -1,16 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 // Verarbeitet die Charaktersteuerung
-public class CharacterMovement : MonoBehaviour {
+public class CharacterMovement : MonoBehaviour
+{
+    private GameObject Player;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Kitchentools will be saved in a List
+    // All Kitchentools have to be tagged with "Kitchentool"
+    private List<GameObject> LKitchentools;
+
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        if (Player != null)
+            Debug.Log("Find Player");
+
+        LKitchentools = new List<GameObject>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
